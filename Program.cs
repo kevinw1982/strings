795ddfcfca768c90c0fc2aa5fa62bb1073a9aa53
Kevin Wainwright <mr_rich199@hotmail.com>
2022-03-04 11:08:45 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Kevin";
+            string message = "My Name is " + myname;
+            string capsMessage = message.ToUpper();
+            string lowMessage = message.ToLower();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(message);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lowMessage);
+            Console.Read();
+        }
+    }
+}
